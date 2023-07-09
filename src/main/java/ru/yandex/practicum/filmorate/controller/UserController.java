70f9fa2fe6963d62adc96e77cb591f753b1e9aaa
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-07-09 20:55:41 +0300

    sprint 9 tz 1.6

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exeption.InvalidUserPropertiesException;
 import ru.yandex.practicum.filmorate.exeption.UserAlreadyExistException;
-import ru.yandex.practicum.filmorate.model.FilmDto;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.model.UserDto;
 import ru.yandex.practicum.filmorate.model.UserInterface;
