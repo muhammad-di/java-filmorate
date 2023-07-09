808b4352d06e7af451d9547daf17d7540e9f508a
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-07-09 18:27:49 +0300

    feat: проведено логирование для UserController, FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exeption.FilmAlreadyExistException;
 import ru.yandex.practicum.filmorate.exeption.InvalidFilmPropertiesException;
@@ -8,6 +9,7 @@ import ru.yandex.practicum.filmorate.validation.FilmValidation;
 
 import java.util.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -21,21 +23,26 @@ public class FilmController {
     @PostMapping
     public Film create(@RequestBody Film film) throws InvalidFilmPropertiesException, FilmAlreadyExistException {
         if (FilmValidation.validate(film)) {
+            log.info("film validation error");
             throw new InvalidFilmPropertiesException("Invalid name of a film", 406);
         }
         if (films.containsKey(film.getId())) {
+            log.info("Film already exists error");
             throw new FilmAlreadyExistException("Film already exists", 409);
         }
         films.put(film.getId(), film);
+        log.info("Film entity with id {} and name {} was created", film.getId(), film.getName());
         return films.get(film.getId());
     }
 
     @PutMapping
     public Film update(@RequestBody Film film) throws InvalidFilmPropertiesException {
         if (FilmValidation.validate(film)) {
+            log.info("film validation error");
             throw new InvalidFilmPropertiesException("Invalid name if a film", 406);
         }
         films.put(film.getId(), film);
+        log.info("Film entity with id {} and name {} was updated", film.getId(), film.getName());
         return films.get(film.getId());
     }
 }
