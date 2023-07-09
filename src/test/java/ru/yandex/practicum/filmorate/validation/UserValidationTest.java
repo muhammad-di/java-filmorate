bf335724be1b4c880962383086b9e20ecdc7de02
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-07-09 20:16:10 +0300

    sprint 9 tz 1.4

--- src/test/java/ru/yandex/practicum/filmorate/validation/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validation/UserValidationTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.validation;
 
-import org.assertj.core.error.ShouldBeTrue;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.User;
 
