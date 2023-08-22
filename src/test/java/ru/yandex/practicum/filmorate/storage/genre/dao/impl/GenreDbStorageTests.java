c6ff0432d823cf3b54b771c68b6e8aa614f650cc
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-08-22 19:44:58 +0300

    sprint 11 tz 1.2

--- src/test/java/ru/yandex/practicum/filmorate/storage/genre/dao/impl/GenreDbStorageTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/genre/dao/impl/GenreDbStorageTests.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
