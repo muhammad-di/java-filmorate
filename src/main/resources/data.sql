2b12be0273b93844fe90bc09208266baa226efa9
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-08-22 20:07:51 +0300

    sprint 11 tz 1.4

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
-INSERT INTO film (NAME, RELEASE_DATE, DURATION, DESCRIPTION, MPA)
-VALUES ('Lucky Number Slevin', '2006-02-24', 110,
-'Description of a Lucky Number Slevin movie', 4
-);
-
-INSERT INTO film (NAME, RELEASE_DATE, DURATION, DESCRIPTION, MPA)
-VALUES ('No Reservations', '2007-07-27', 104,
-'Description of a No Reservations movie', 2
-);
+--INSERT INTO film (NAME, RELEASE_DATE, DURATION, DESCRIPTION, MPA)
+--VALUES ('Lucky Number Slevin', '2006-02-24', 110,
+--'Description of a Lucky Number Slevin movie', 4
+--);
 --
-INSERT INTO film (NAME, RELEASE_DATE, DURATION, DESCRIPTION, MPA)
-VALUES ('Dune', '2021-10-22', 155,
-'Description of a Dune movie', 3
-);
+--INSERT INTO film (NAME, RELEASE_DATE, DURATION, DESCRIPTION, MPA)
+--VALUES ('No Reservations', '2007-07-27', 104,
+--'Description of a No Reservations movie', 2
+--);
+----
+--INSERT INTO film (NAME, RELEASE_DATE, DURATION, DESCRIPTION, MPA)
+--VALUES ('Dune', '2021-10-22', 155,
+--'Description of a Dune movie', 3
+--);
 --
 INSERT INTO mpa (NAME) VALUES ('G');
 INSERT INTO mpa (NAME) VALUES ('PG');
@@ -30,26 +30,26 @@ INSERT INTO genre (NAME) VALUES ('Мультфильм');
 INSERT INTO genre (NAME) VALUES ('Триллер');
 INSERT INTO genre (NAME) VALUES ('Документальный');
 INSERT INTO genre (NAME) VALUES ('Боевик');
-INSERT INTO genre (NAME) VALUES ('DOCUMENTARY');
-INSERT INTO genre (NAME) VALUES ('DRAMA');
-INSERT INTO genre (NAME) VALUES ('FAMILY');
-INSERT INTO genre (NAME) VALUES ('FANTASY');
-INSERT INTO genre (NAME) VALUES ('HISTORY');
-INSERT INTO genre (NAME) VALUES ('HORROR');
-INSERT INTO genre (NAME) VALUES ('KIDS');
-INSERT INTO genre (NAME) VALUES ('MUSIC');
-INSERT INTO genre (NAME) VALUES ('MYSTERY');
-INSERT INTO genre (NAME) VALUES ('NEWS');
-INSERT INTO genre (NAME) VALUES ('REALITY');
-INSERT INTO genre (NAME) VALUES ('ROMANCE');
-INSERT INTO genre (NAME) VALUES ('SCI_FI');
-INSERT INTO genre (NAME) VALUES ('SCIENCE_FICTION');
-INSERT INTO genre (NAME) VALUES ('SOAP');
-INSERT INTO genre (NAME) VALUES ('TALK');
-INSERT INTO genre (NAME) VALUES ('THRILLER');
-INSERT INTO genre (NAME) VALUES ('WAR');
-INSERT INTO genre (NAME) VALUES ('POLITICS');
-INSERT INTO genre (NAME) VALUES ('WESTERN');
+--INSERT INTO genre (NAME) VALUES ('DOCUMENTARY');
+--INSERT INTO genre (NAME) VALUES ('DRAMA');
+--INSERT INTO genre (NAME) VALUES ('FAMILY');
+--INSERT INTO genre (NAME) VALUES ('FANTASY');
+--INSERT INTO genre (NAME) VALUES ('HISTORY');
+--INSERT INTO genre (NAME) VALUES ('HORROR');
+--INSERT INTO genre (NAME) VALUES ('KIDS');
+--INSERT INTO genre (NAME) VALUES ('MUSIC');
+--INSERT INTO genre (NAME) VALUES ('MYSTERY');
+--INSERT INTO genre (NAME) VALUES ('NEWS');
+--INSERT INTO genre (NAME) VALUES ('REALITY');
+--INSERT INTO genre (NAME) VALUES ('ROMANCE');
+--INSERT INTO genre (NAME) VALUES ('SCI_FI');
+--INSERT INTO genre (NAME) VALUES ('SCIENCE_FICTION');
+--INSERT INTO genre (NAME) VALUES ('SOAP');
+--INSERT INTO genre (NAME) VALUES ('TALK');
+--INSERT INTO genre (NAME) VALUES ('THRILLER');
+--INSERT INTO genre (NAME) VALUES ('WAR');
+--INSERT INTO genre (NAME) VALUES ('POLITICS');
+--INSERT INTO genre (NAME) VALUES ('WESTERN');
 --
 --INSERT INTO film_genre (FILM_ID, GENRE_ID) VALUES (1,6);
 --INSERT INTO film_genre (FILM_ID, GENRE_ID) VALUES (1,8);
@@ -59,16 +59,16 @@ INSERT INTO genre (NAME) VALUES ('WESTERN');
 --INSERT INTO film_genre (FILM_ID, GENRE_ID) VALUES (2,8);
 --INSERT INTO film_genre (FILM_ID, GENRE_ID) VALUES (2,18);
 ----
-insert into users (login, birthday, email, name) values ('bphripp0', '1980-12-25', 'bruxton0@chicagotribune.com', 'Brett');
-insert into users (login, birthday, email, name) values ('dmccrone1', '1984-01-21', 'dskellon1@columbia.edu', 'Dru');
-insert into users (login, birthday, email, name) values ('svaszoly2', '1979-03-20', 'swalcher2@newsvine.com', 'Sarina');
-insert into users (login, birthday, email, name) values ('jwood3', '1978-03-24', 'jfletham3@yolasite.com', 'Jard');
-insert into users (login, birthday, email, name) values ('rlevington4', '1979-09-09', 'rclough4@seattletimes.com', 'Randi');
-insert into users (login, birthday, email, name) values ('nmartel5', '1965-12-04', 'ngosswell5@pen.io', 'Nichols');
-insert into users (login, birthday, email, name) values ('ccrosen6', '1960-09-11', 'cgerman6@live.com', 'Clarie');
-insert into users (login, birthday, email, name) values ('wcolbron7', '1975-06-07', 'wrobardey7@cbsnews.com', 'Wesley');
-insert into users (login, birthday, email, name) values ('ldunstall8', '1981-07-20', 'lhiseman8@home.pl', 'Lazare');
-insert into users (login, birthday, email, name) values ('ahurley9', '1995-11-22', 'aboneham9@wiley.com', 'Alick');
+--insert into users (login, birthday, email, name) values ('bphripp0', '1980-12-25', 'bruxton0@chicagotribune.com', 'Brett');
+--insert into users (login, birthday, email, name) values ('dmccrone1', '1984-01-21', 'dskellon1@columbia.edu', 'Dru');
+--insert into users (login, birthday, email, name) values ('svaszoly2', '1979-03-20', 'swalcher2@newsvine.com', 'Sarina');
+--insert into users (login, birthday, email, name) values ('jwood3', '1978-03-24', 'jfletham3@yolasite.com', 'Jard');
+--insert into users (login, birthday, email, name) values ('rlevington4', '1979-09-09', 'rclough4@seattletimes.com', 'Randi');
+--insert into users (login, birthday, email, name) values ('nmartel5', '1965-12-04', 'ngosswell5@pen.io', 'Nichols');
+--insert into users (login, birthday, email, name) values ('ccrosen6', '1960-09-11', 'cgerman6@live.com', 'Clarie');
+--insert into users (login, birthday, email, name) values ('wcolbron7', '1975-06-07', 'wrobardey7@cbsnews.com', 'Wesley');
+--insert into users (login, birthday, email, name) values ('ldunstall8', '1981-07-20', 'lhiseman8@home.pl', 'Lazare');
+--insert into users (login, birthday, email, name) values ('ahurley9', '1995-11-22', 'aboneham9@wiley.com', 'Alick');
 --
 --INSERT INTO likes (FILM_ID, USER_ID) VALUES (2,16);
 --INSERT INTO likes (FILM_ID, USER_ID) VALUES (2,22);
--- src/test/java/ru/yandex/practicum/filmorate/storage/mpa/dao/impl/MpaDbStorageTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/mpa/dao/impl/MpaDbStorageTests.java
@@ -1,61 +1,61 @@
-package ru.yandex.practicum.filmorate.storage.mpa.dao.impl;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.Mpa;
-
-import java.util.*;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@Slf4j
-@SpringBootTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-public class MpaDbStorageTests {
-    private final MpaDbStorage mpaDbStorage;
-
-    @Test
-    public void testFindAll() {
-        Collection<Mpa> expected =  List.of(new Mpa(1, "G"),
-                new Mpa(2, "PG"),
-                new Mpa(3, "PG-13"),
-                new Mpa(4, "R"),
-                new Mpa(5, "NC-17")
-                );
-
-        Optional<Collection<Mpa>> mpaOptional = Optional.ofNullable(mpaDbStorage.findAll());
-        assertThat(mpaOptional).isPresent().hasValue(expected);
-    }
-
-    @Test
-    public void testFindMpaById() {
-        Mpa expected = new Mpa(1, "G");
-
-        Optional<Mpa> mpaOptional = Optional.ofNullable(mpaDbStorage.getMpaById(1));
-        assertThat(mpaOptional).isPresent().hasValue(expected);
-    }
-
-
-    @Test
-    public void testContainsMpa() {
-        Boolean expected = true;
-
-        Optional<Boolean> mpaOptional = Optional.of(mpaDbStorage.containsMpa(2));
-        assertThat(mpaOptional).isPresent().hasValue(expected);
-        log.info("----------ContainsMpa {}", mpaOptional.get());
-    }
-
-    @Test
-    public void testContainsFilmFalse() {
-        Boolean expected = false;
-
-        Optional<Boolean> mpaOptional = Optional.of(mpaDbStorage.containsMpa(7));
-        assertThat(mpaOptional).isPresent().hasValue(expected);
-        log.info("----------ContainsFilm {}", mpaOptional.get());
-    }
-}
+//package ru.yandex.practicum.filmorate.storage.mpa.dao.impl;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import ru.yandex.practicum.filmorate.model.Mpa;
+//
+//import java.util.*;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//@Slf4j
+//@SpringBootTest
+//@AutoConfigureTestDatabase
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
+//public class MpaDbStorageTests {
+//    private final MpaDbStorage mpaDbStorage;
+//
+//    @Test
+//    public void testFindAll() {
+//        Collection<Mpa> expected =  List.of(new Mpa(1, "G"),
+//                new Mpa(2, "PG"),
+//                new Mpa(3, "PG-13"),
+//                new Mpa(4, "R"),
+//                new Mpa(5, "NC-17")
+//                );
+//
+//        Optional<Collection<Mpa>> mpaOptional = Optional.ofNullable(mpaDbStorage.findAll());
+//        assertThat(mpaOptional).isPresent().hasValue(expected);
+//    }
+//
+//    @Test
+//    public void testFindMpaById() {
+//        Mpa expected = new Mpa(1, "G");
+//
+//        Optional<Mpa> mpaOptional = Optional.ofNullable(mpaDbStorage.getMpaById(1));
+//        assertThat(mpaOptional).isPresent().hasValue(expected);
+//    }
+//
+//
+//    @Test
+//    public void testContainsMpa() {
+//        Boolean expected = true;
+//
+//        Optional<Boolean> mpaOptional = Optional.of(mpaDbStorage.containsMpa(2));
+//        assertThat(mpaOptional).isPresent().hasValue(expected);
+//        log.info("----------ContainsMpa {}", mpaOptional.get());
+//    }
+//
+//    @Test
+//    public void testContainsFilmFalse() {
+//        Boolean expected = false;
+//
+//        Optional<Boolean> mpaOptional = Optional.of(mpaDbStorage.containsMpa(7));
+//        assertThat(mpaOptional).isPresent().hasValue(expected);
+//        log.info("----------ContainsFilm {}", mpaOptional.get());
+//    }
+//}
