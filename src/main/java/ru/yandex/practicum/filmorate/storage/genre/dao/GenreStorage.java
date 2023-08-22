ed4485b8d1bca9f72e4ecff0f2675849e880f3d9
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-08-22 20:22:37 +0300

    sprint 11 tz 1.6

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/dao/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/dao/GenreStorage.java
@@ -2,10 +2,10 @@ package ru.yandex.practicum.filmorate.storage.genre.dao;
 
 import ru.yandex.practicum.filmorate.model.Genre;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface GenreStorage {
-    Collection<Genre> findAll();
+    List<Genre> findAll();
 
     Genre getGenreById(Integer id);
 
