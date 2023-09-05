8de79e1bff305fb72a0a99c9b0ad0a67fcdfaaec
Svetlana <tchmykova@yandex.ru>
2023-09-05 14:45:33 +0500

    fix: formatted

--- src/main/java/ru/yandex/practicum/filmorate/Constants.java
+++ src/main/java/ru/yandex/practicum/filmorate/Constants.java
@@ -1,8 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-
-import org.springframework.beans.factory.annotation.Value;
-
 public class Constants {
     public static final Integer MIN_ID = 1;
     public static final String LIKES_SORT = "likes";
